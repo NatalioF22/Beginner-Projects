5412a458cc39d1f8f3af81e7028b1deb59d45d12
Natalio Fernandes <116610989+NatalioF22@users.noreply.github.com>
2022-10-28 17:40:13 -0400

    Update main.py

--- Graphical User Interface/Leap Year/main.py
+++ Graphical User Interface/Leap Year/main.py
@@ -7,30 +7,34 @@ app.maxsize(500,200)
 welcome = Label(app, text = "Welcome to Leap It\nEnter a year and we'll tell you if it's Leap year or not.")
 welcome.pack()
 
-year = Entry(app)
-year.pack()
+class Window:
+    def __init__(self,master):
+        self.year = Entry(master)
+        self.year.pack()
 
-def Leap():
-    #Check error missing
-    info.delete('0',END)
-    value =  int(year.get())
-    if (value %4) ==0:
-        if value %100==0:
-            if value%400==0:
-                info.insert(INSERT,'Leap')
+        self.Check = Button(master, text = "Check", command = self.Leap)
+        self.Check.pack()
+
+        self.info = Entry(master)
+        self.info.pack()
+
+    def Leap(self):
+        #Check error missing
+        self.info.delete('0',END)
+        value =  int(self.year.get())
+        if (value %4) ==0:
+            if value %100==0:
+                if value%400==0:
+                    self.info.insert(INSERT,'Leap')
+                else:
+                    self.info.insert(INSERT,'Not Leap')
             else:
-                info.insert(INSERT,'Not Leap')
+                self.info.insert(INSERT,'Leap')
         else:
-            info.insert(INSERT,'Leap')
-    else:
-        info.insert(INSERT,'Not Leap') 
-    
-Check = Button(app, text = "Check", command = Leap)
-Check.pack()
+            self.info.insert(INSERT,'Not Leap') 
+            
+        
 
-info = Entry(app)
-info.pack()
-
-
+window_obj = Window(app)
 
 app.mainloop()
