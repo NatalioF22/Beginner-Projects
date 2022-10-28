82a1b19f28b9bff16d857b75d934cad111910af7
Natalio Fernandes <116610989+NatalioF22@users.noreply.github.com>
2022-10-28 17:38:07 -0400

    Update main.py

--- Graphical User Interface/Age Calculator/main.py
+++ Graphical User Interface/Age Calculator/main.py
@@ -7,46 +7,48 @@ app.geometry('370x300')
 app.minsize(370,300)
 app.maxsize(370,300)
 
+class AgeCal:
+    def __init__(self,tkinterwindow):
+        self.welcome = Label(tkinterwindow, text = "Calculate your age",pady = 20,padx = 20)
+        self.welcome.grid(row=0, column = 1,columnspan=2)
 
-welcome = Label(app, text = "Calculate your age",pady = 20,padx = 20)
-welcome.grid(row=0, column = 1,columnspan=2)
+        self.year = Label(tkinterwindow,text = "Birth Year",padx = 20)
+        self.year.grid(row = 1, column = 0)
 
-year = Label(app,text = "Birth Year",padx = 20)
-year.grid(row = 1, column = 0)
+        self.month = Label(tkinterwindow,text = "Birth Month")
+        self.month.grid(row = 2, column = 0)
 
-month = Label(app,text = "Birth Month")
-month.grid(row = 2, column = 0)
-
-day = Label(app,text = "Birth Day")
-day.grid(row = 3, column = 0)
+        self.day = Label(tkinterwindow,text = "Birth Day")
+        self.day.grid(row = 3, column = 0)
 
 
-#Entries
-year_entry = Entry(app, )
-year_entry.grid(row = 1, column = 1)
+        #Entries
+        self.year_entry = Entry(tkinterwindow, )
+        self.year_entry.grid(row = 1, column = 1)
 
-month_entry = Entry(app)
-month_entry.grid(row = 2, column = 1)
+        self.month_entry = Entry(tkinterwindow)
+        self.month_entry.grid(row = 2, column = 1)
 
-day_entry = Entry(app)
-day_entry.grid(row = 3, column = 1)
+        self.day_entry = Entry(tkinterwindow)
+        self.day_entry.grid(row = 3, column = 1)
 
-show = Text(app,width = 4, height=1, font = ('arial',40))
-show.grid(row = 5, column = 1, columnspan=2)
+        self.show = Text(tkinterwindow,width = 4, height=1, font = ('arial',40))
+        self.show.grid(row = 5, column = 1, columnspan=2)
 
-#Buttom
-def calculate():
-    show.delete('1.0',END)
-    today = date.today()
-    year = int(year_entry.get())
-    month = int(month_entry.get())
-    day = int(day_entry.get())
-    AGE  = today.year - year -((today.month,month)<( today.day,day))
-    show.insert(INSERT,AGE)
+        self.Calculate = Button(app,text = "Calculate", command = self.calculate)
+        self.Calculate.grid(row = 4, column = 1, columnspan=2)
 
-#definbe a function to check the error missing
-    
+    #Buttom
+    def calculate(self):
+        self.show.delete('1.0',END)
+        today = date.today()
+        year = int(self.year_entry.get())
+        month = int(self.month_entry.get())
+        day = int(self.day_entry.get())
+        AGE  = today.year - year -((today.month,month)<( today.day,day))
+        self.show.insert(INSERT,AGE)
 
-Calculate = Button(app,text = "Calculate", command = calculate)
-Calculate.grid(row = 4, column = 1, columnspan=2)
+    #definbe a function to check the error missing
+        
+test = AgeCal(app)
 app.mainloop()
