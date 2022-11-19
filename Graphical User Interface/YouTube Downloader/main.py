753226d57b4d81481aae0525751a46e07007c6d4
Natalio Fernandes <116610989+NatalioF22@users.noreply.github.com>
2022-11-19 00:52:54 -0500

    Update main.py

--- Graphical User Interface/YouTube Downloader/main.py
+++ Graphical User Interface/YouTube Downloader/main.py
@@ -1,10 +1,7 @@
-from msilib.schema import Font
-from pytube import YouTube
 from tkinter import *
-from tkinter import messagebox
+
 from Classes import Functions
+
 app = Tk()
-
-
 obj = Functions(app)
 app.mainloop()
